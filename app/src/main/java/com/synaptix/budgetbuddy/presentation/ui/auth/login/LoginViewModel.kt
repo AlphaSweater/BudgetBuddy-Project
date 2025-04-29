60e63d03b6840edb3c5a13168f35671a6574cec6
Entropy-3 <Nathanteixeira981@gmail.com>
2025-04-29 18:02:03 +0200

    Refactor: Use Hilt for LoginViewModel and LoginFragment
    - Updated `LoginViewModel` to use `@HiltViewModel` and constructor injection.
    - Injected `LoginUseCase` into `LoginViewModel`.
    - Implemented actual login logic with `LoginUseCase` in `LoginViewModel`.
    - Added `@AndroidEntryPoint` to `LoginFragment` to enable Hilt injection.
    - Removed unnecessary `LoginViewModel` code from `LandingViewModel` class.

--- app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/auth/login/LoginViewModel.kt
@@ -3,18 +3,24 @@ package com.synaptix.budgetbuddy.presentation.ui.auth.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.synaptix.budgetbuddy.core.usecase.auth.LoginUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : ViewModel() {
 
     private val _loginResult = MutableLiveData<Boolean>()
     val loginResult: LiveData<Boolean> get() = _loginResult
 
     fun login(email: String, password: String) {
-        // TEMPORARY HARD-CODED login logic
-        if (email == "admin" && password == "admin") {
-            _loginResult.value = true
-        } else {
-            _loginResult.value = false
+        viewModelScope.launch {
+            val result = loginUseCase.execute(email, password)
+            _loginResult.postValue(result)
         }
     }
 }
