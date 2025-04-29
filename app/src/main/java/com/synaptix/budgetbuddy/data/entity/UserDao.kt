58176977a21ad519efcda8d810c38843d635ed79
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2025-04-29 13:39:53 +0200

    Feature/databse/nathan (#23)
    * Add UserEntity and UserDao
    
    - Create UserEntity for the database.
    - Create UserDao for database interactions.
    
    * Add CategoryDao and CategoryEntity for Room database
    
    This commit introduces the `CategoryDao` interface for data access and the `CategoryEntity` data class for representing categories within the Room database. `CategoryDao` includes methods for inserting and retrieving categories by ID, while `CategoryEntity` defines the structure of a category record in the database.
    
    * Add WalletEntity and WalletDao
    
    - Added `WalletEntity` for wallet data storage with foreign key to `UserEntity`.
    - Added `WalletDao` with `insert` and `getWalletById` functions.
    - `getWalletById` is a SQL query to retrieve a wallet based on `wallet_id`.
    
    * feat: Add TransactionDao, TransactionEntity, and update UserEntity and UserDao
    
    - Add `TransactionDao` interface for database operations related to transactions.
    - Add `TransactionEntity` data class for representing transaction data in the database.
    - Move `UserEntity` to the `local` package.
    - Move `UserDao` to the `entity` package.
    - Update the `TransactionEntity` to include foreign keys for `UserEntity` and `WalletEntity`.
    
    * Add Room database and update deployment target selector

---
+++ app/src/main/java/com/synaptix/budgetbuddy/data/entity/UserDao.kt
@@ -0,0 +1,18 @@
+package com.synaptix.budgetbuddy.data.entity
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.synaptix.budgetbuddy.data.local.UserEntity
+
+@Dao
+interface UserDao {
+    //inerts a user into the database
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: UserEntity): Long
+
+    //sql query to grab a user based on user ID
+    @Query("SELECT * FROM user_table WHERE user_id = :userId")
+    suspend fun getUserById(userId: Int): UserEntity?
+}
