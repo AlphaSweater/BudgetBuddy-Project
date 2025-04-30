cee78473129c24c8d777208a766f43fc6fb80d2b
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2025-04-30 14:43:57 +0200

    Feature/database logic/nathan (#30)
    * Add a foreign key for category and update TransactionEntity
    
    The TransactionEntity class was updated to add a foreign key constraint referencing the CategoryEntity.
    Also TransactionEntity class has been updated by adding new columns.
    The selected tab id for the app insights was set to Firebase Crashlytics.
    
    * Define CategoryEntity as a Room entity with foreign key to UserEntity.
    
    * feat: Add BudgetEntity and BudgetDao
    
    - Add `BudgetEntity` with foreign keys to `UserEntity` and `WalletEntity`.
    - Add `BudgetDao` with methods to insert a budget, get a budget by ID, get budgets by user ID, and get budgets by wallet ID.
    - Update `CategoryEntity` to use the `ForeignKey` annotation.
    
    * Update AppDatabase entities and version
    
    Add BudgetEntity to the list of entities for the database.
    Increment the database version from 2 to 3.

--- app/src/main/java/com/synaptix/budgetbuddy/data/entity/CategoryEntity.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/data/entity/CategoryEntity.kt
@@ -2,11 +2,22 @@ package com.synaptix.budgetbuddy.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
 @Entity(
-    tableName = "category_table"
+    tableName = "category_table",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["user_id"],
+            childColumns = ["user_id"]
+        ),
+    ]
 )
 data class CategoryEntity (
     @PrimaryKey(autoGenerate = true) val category_id: Int,
-    val name: String
+    val user_id: Int,
+    val name: String,
+    val colour: String,
+    val icon: String
 )
