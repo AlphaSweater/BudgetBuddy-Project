df904362fada5b95e5f9064b70ae222993f0fac8
Entropy-3 <Nathanteixeira981@gmail.com>
2025-04-30 14:26:46 +0200

    Define CategoryEntity as a Room entity with foreign key to UserEntity.

--- app/src/main/java/com/synaptix/budgetbuddy/data/entity/CategoryEntity.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/data/entity/CategoryEntity.kt
@@ -4,9 +4,19 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "category_table"
+    tableName = "category_table",
+    foreignKeys = [
+        androidx.room.ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["user_id"],
+            childColumns = ["user_id"]
+        ),
+    ]
 )
 data class CategoryEntity (
     @PrimaryKey(autoGenerate = true) val category_id: Int,
-    val name: String
+    val user_id: Int,
+    val name: String,
+    val colour: String,
+    val icon: String
 )
