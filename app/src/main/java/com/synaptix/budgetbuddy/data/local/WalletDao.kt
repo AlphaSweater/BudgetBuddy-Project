26aa0f643c07c20ce6a04bc41c25aae3a3a3fce0
Entropy-3 <Nathanteixeira981@gmail.com>
2025-04-30 11:19:18 +0200

    Add balance to WalletEntity and add getWalletsByUserId query to WalletDao.

--- app/src/main/java/com/synaptix/budgetbuddy/data/local/WalletDao.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/data/local/WalletDao.kt
@@ -14,4 +14,8 @@ interface WalletDao {
     //sql query to grab a wallet based on wallet ID
     @Query("SELECT * FROM wallet_table WHERE wallet_id = :walletId")
     suspend fun getWalletById(walletId: Int): WalletEntity?
+
+    //sql query to grab all wallets based on user ID
+    @Query("SELECT * FROM wallet_table WHERE user_id = :userId")
+    suspend fun getWalletsByUserId(userId: Int): List<WalletEntity>
 }
