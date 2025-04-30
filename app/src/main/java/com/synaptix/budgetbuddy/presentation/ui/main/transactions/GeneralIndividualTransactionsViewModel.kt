0a1f00395ad942a473bd695e5e5608f3a603bbc0
Kayla Ferreira <ST10259527@vcconnect.edu.za>
2025-04-30 16:44:30 +0200

    Remove general individual transaction fragment
    *   Deleted `fragment_general_individual_transactions.xml`
    *   Removed `GeneralIndividualTransactionsFragment`
    *   Removed `GeneralIndividualTransactionsViewModel`
    
    MY BAD

--- app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/transactions/GeneralIndividualTransactionsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.synaptix.budgetbuddy.presentation.ui.main.transactions
-
-import androidx.lifecycle.ViewModel
-
-class GeneralIndividualTransactionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
