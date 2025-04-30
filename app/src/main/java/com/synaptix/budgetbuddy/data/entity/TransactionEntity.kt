071c62f5e7ad0775c7f79dcb3c732ad237881704
Entropy-3 <Nathanteixeira981@gmail.com>
2025-04-30 14:24:31 +0200

    Add a foreign key for category and update TransactionEntity
    The TransactionEntity class was updated to add a foreign key constraint referencing the CategoryEntity.
    Also TransactionEntity class has been updated by adding new columns.
    The selected tab id for the app insights was set to Firebase Crashlytics.

--- app/src/main/java/com/synaptix/budgetbuddy/data/entity/TransactionEntity.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/data/entity/TransactionEntity.kt
@@ -3,7 +3,6 @@ package com.synaptix.budgetbuddy.data.entity
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-
 @Entity(
     tableName = "transaction_table",
     //AI assisted with the creation of this foreign key
@@ -17,12 +16,26 @@ import androidx.room.PrimaryKey
             entity = WalletEntity::class,
             parentColumns = ["wallet_id"],
             childColumns = ["wallet_id"]
-        )
+        ),
+        ForeignKey(
+        entity = CategoryEntity::class,
+        parentColumns = ["category_id"],
+        childColumns = ["category_id"]
+)
     ]
 )
 
 data class TransactionEntity (
     @PrimaryKey(autoGenerate = true) val transaction_id: Int,
     val user_id: Int,
-    val wallet_id: Int
+    val wallet_id: Int,
+    val category_id: Int,
+    val amount: Double,
+    val date: String,
+    val note: String,
+    val currency: String,
+    val label: String,
+    val image: String,
+    val recurrence: String
+
 )
