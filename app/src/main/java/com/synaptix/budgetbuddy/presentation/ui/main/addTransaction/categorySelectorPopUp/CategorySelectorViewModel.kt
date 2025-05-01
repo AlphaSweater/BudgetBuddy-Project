5125dc56201b8517806bcbe57cf7964634b46a7e
Entropy-3 <Nathanteixeira981@gmail.com>
2025-05-01 17:09:53 +0200

    feat: Implement GetCategoriesUseCase and integrate with CategorySelectorViewModel
    - Create `GetCategoriesUseCase` to fetch categories from `CategoryRepository`.
    - Create `CategoryRepository` to access `CategoryDao`.
    - Modify `CategoryDao` to retrieve categories by user ID.
    - Inject `GetCategoriesUseCase` into `CategorySelectorViewModel`.
    - Update `CategoryAdapter` to add space in the last element.

--- app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/addTransaction/categorySelectorPopUp/CategorySelectorViewModel.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/addTransaction/categorySelectorPopUp/CategorySelectorViewModel.kt
@@ -1,7 +1,10 @@
 package com.synaptix.budgetbuddy.presentation.ui.main.addTransaction.categorySelectorPopUp
 
 import androidx.lifecycle.ViewModel
+import com.synaptix.budgetbuddy.core.usecase.main.transaction.GetCategoriesUseCase
+import javax.inject.Inject
 
-class CategorySelectorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CategorySelectorViewModel @Inject constructor(
+    private val getCategoriesUseCase: GetCategoriesUseCase
+): ViewModel() {
 }
