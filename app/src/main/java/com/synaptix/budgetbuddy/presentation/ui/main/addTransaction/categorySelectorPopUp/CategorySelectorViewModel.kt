724a1a34617b4ee53ea8b55b2adf995dc678f173
Entropy-3 <Nathanteixeira981@gmail.com>
2025-05-01 19:02:38 +0200

    feat: Load and display categories from database
    - Implemented loading of categories from the database in `CategorySelectorViewModel` using `GetCategoriesUseCase`.
    - Added a `loadCategories` function in `CategorySelectorViewModel` to fetch categories for a given user ID.
    - Implemented LiveData to observe changes in the categories list.
    - In `CategorySelectorFragment`, categories are loaded on fragment creation and displayed in the corresponding RecyclerViews.
    - Updated `CategorySelectorFragment` to use `viewModels()` for `CategorySelectorViewModel`.
    - Added Hilt for Dependency injection in `CategorySelectorFragment` and `CategorySelectorViewModel`.
    - Created `getDrawableId` and `getColorId` utility functions in `CategorySelectorFragment`.
    - Updated `AppModule` to provide `CategoryDao`.
    - Added `instantiateDBS` function to `CategorySelectorFragment`.
    - Created `getUserId` function to `CategorySelectorFragment` that return the current `userId`.

--- app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/addTransaction/categorySelectorPopUp/CategorySelectorViewModel.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/addTransaction/categorySelectorPopUp/CategorySelectorViewModel.kt
@@ -1,10 +1,26 @@
 package com.synaptix.budgetbuddy.presentation.ui.main.addTransaction.categorySelectorPopUp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.synaptix.budgetbuddy.core.usecase.main.transaction.GetCategoriesUseCase
+import com.synaptix.budgetbuddy.data.entity.CategoryEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class CategorySelectorViewModel @Inject constructor(
     private val getCategoriesUseCase: GetCategoriesUseCase
 ): ViewModel() {
+    private val _categories = MutableLiveData<List<CategoryEntity>>()
+    val categories: LiveData<List<CategoryEntity>> get() = _categories
+
+    fun loadCategories(userId: Int) {
+        viewModelScope.launch {
+            val result = getCategoriesUseCase.invoke(userId)
+            _categories.value = result
+        }
+    }
 }
