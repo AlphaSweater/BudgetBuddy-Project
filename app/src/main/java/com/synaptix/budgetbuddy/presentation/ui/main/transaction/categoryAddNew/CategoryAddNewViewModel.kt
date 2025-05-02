bb18529dc7d64ccbb8ea312e82f5302a54a7d87d
Kayla Ferreira <ST10259527@vcconnect.edu.za>
2025-05-02 22:58:56 +0200

    feat: Add Category Creation
    *   Implement AddCategoryUseCase for adding new categories.
    *   Add navigation and UI for category creation in CategoryAddNewFragment.
    *   Add CategoryAddNewViewModel for managing category creation logic.
    *   Update database version to 9.
    *   Add methods to CategoryRepository for adding categories.
    * Add arguments to navigation.
    * Add icons for new category
    * Add colors for new category
    * Add go back to category view

--- app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/transaction/categoryAddNew/CategoryAddNewViewModel.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/transaction/categoryAddNew/CategoryAddNewViewModel.kt
@@ -1,7 +1,53 @@
 package com.synaptix.budgetbuddy.presentation.ui.main.transaction.categoryAddNew
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.synaptix.budgetbuddy.core.model.CategoryIn
+import com.synaptix.budgetbuddy.core.usecase.auth.GetUserIdUseCase
+import com.synaptix.budgetbuddy.core.usecase.main.transaction.AddCategoryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CategoryAddNewViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class CategoryAddNewViewModel @Inject constructor(
+    private val addCategoryUseCase: AddCategoryUseCase,
+    private val getUserIdUseCase: GetUserIdUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val currentUserId: Int = savedStateHandle["currentUserId"] ?: 0
+
+    val categoryName = MutableLiveData<String>()
+    val selectedIcon = MutableLiveData<Int>() // Drawable resource ID
+    val selectedColor = MutableLiveData<Int>() // Color int value
+    val categoryType = MutableLiveData("Expense")
+
+    private val _eventCategoryCreated = MutableLiveData<Boolean>()
+    val eventCategoryCreated: LiveData<Boolean> = _eventCategoryCreated
+
+    fun createCategory() {
+        val name = categoryName.value
+        val icon = selectedIcon.value
+        val color = selectedColor.value
+        val type = categoryType.value ?: "Expense"
+
+        if (!name.isNullOrBlank() && icon != null && color != null) {
+            viewModelScope.launch {
+                val userId = getUserIdUseCase.execute()
+
+                val newCategory = CategoryIn(
+                    categoryName = name,
+                    categoryIcon = icon,
+                    categoryColor = color,
+                    categoryType = type,
+                    userId = userId
+                )
+
+                addCategoryUseCase.execute(newCategory)
+                _eventCategoryCreated.value = true
+            }
+        } else {
+            _eventCategoryCreated.value = false
+        }
+    }
 }
