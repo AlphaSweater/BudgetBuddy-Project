625a4309e3d834b97ce77d3a127234839b9a9642
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2025-05-02 21:09:57 +0200

    Feature/display t ranmsactions/nathan (#80)
    * Update configuration for deployment and modify BudgetMainFragment
    
    - Added a new selection state for the "app" run configuration in `deploymentTargetSelector.xml`.
    - Modified `BudgetMainFragment.kt` to remove commented-out code, and to improve the budget item status and icon assignment.
    
    * feat: Implement GeneralReportsViewModel and update GeneralReportsFragment
    
    - Implemented `GeneralReportsViewModel` to fetch and process transactions.
    - Added `loadTransactions` to `GeneralReportsViewModel` that retrieves transactions, groups them by category, and prepares `BudgetReportListItems.CategoryItems`.
    - Introduced `reportCategoryItems` LiveData to observe category-wise transaction data.
    - Updated `GeneralReportsFragment` to observe `reportCategoryItems` and populate the expense category recycler view.
    - Added `AndroidEntryPoint` to `GeneralReportsFragment` and `BudgetMainFragment` for Hilt dependency injection.
    - Removed hardcoded label and category data from `GeneralReportsFragment` and rely on data from `GeneralReportsViewModel` instead.
    
    * Update budget amount using view model
    
    - Implement `updateBudgetAmount` in `TransactionAddViewModel` to update the budget amount.
    - Add a corresponding `updateAmount` function in `BudgetDao` to update the budget's amount in the database.
    - Add a `updateBudgetAmount` in `BudgetRepository` to call dao.
    
    * Refactor: Remove `updateBudgetAmount` from `BudgetRepository` and `TransactionAddViewModel`
    
    The `updateBudgetAmount` function was removed from both the `BudgetRepository` and `TransactionAddViewModel`. This change removes unnecessary budget amount update logic from these classes.

--- app/src/main/java/com/synaptix/budgetbuddy/data/repository/TransactionRepository.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/data/repository/TransactionRepository.kt
@@ -1,6 +1,9 @@
 package com.synaptix.budgetbuddy.data.repository
 
+import com.synaptix.budgetbuddy.core.model.Transaction
+import com.synaptix.budgetbuddy.core.model.Wallet
 import com.synaptix.budgetbuddy.data.entity.TransactionEntity
+import com.synaptix.budgetbuddy.data.entity.mapper.toDomain
 import com.synaptix.budgetbuddy.data.local.dao.TransactionDao
 import javax.inject.Inject
 
@@ -11,7 +14,9 @@ class TransactionRepository @Inject constructor(
         return transactionDao.insert(entity)
     }
 
-    suspend fun getTransactionsForUser(userId: Int): List<TransactionEntity> {
-        return transactionDao.getTransactionsForUser(userId)
+    suspend fun getTransactionsForUser(userId: Int): List<Transaction> {
+        val transactions = transactionDao.getTransactionsWithDetail(userId)
+        return transactions.map { it.toDomain() }
     }
+
 }
