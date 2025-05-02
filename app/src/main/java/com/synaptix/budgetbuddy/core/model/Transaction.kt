2f7500dd3e7eee64a57244fe76050870e2bd5981
Entropy-3 <Nathanteixeira981@gmail.com>
2025-05-02 10:27:44 +0200

    Add TransactionFull and TransactionWithDetails data classes
    These data classes are created to encapsulate transaction details.
    *  TransactionFull: Represents a complete view of a transaction, including user, wallet, and category information.
    * TransactionWithDetails: Represents the transaction details with the user, wallet and category, for database operation.
    * Added `toTransactionFull` to convert a `TransactionWithDetails` instance into a `TransactionFull` instance.

--- app/src/main/java/com/synaptix/budgetbuddy/core/model/Transaction.kt
+++ app/src/main/java/com/synaptix/budgetbuddy/core/model/Transaction.kt
@@ -1,5 +1,11 @@
 package com.synaptix.budgetbuddy.core.model
 
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.synaptix.budgetbuddy.data.entity.CategoryEntity
+import com.synaptix.budgetbuddy.data.entity.TransactionEntity
+import com.synaptix.budgetbuddy.data.entity.UserEntity
+import com.synaptix.budgetbuddy.data.entity.WalletEntity
 import java.io.Serializable
 
 data class Transaction(
@@ -15,3 +21,54 @@ data class Transaction(
     val photo: ByteArray?,
     val recurrenceRate: String?
 ) : Serializable
+data class TransactionFull(
+    val id: Int,
+    val amount: Double,
+    val date: String,
+    val note: String,
+    val currency: String,
+    val label: String,
+    val image: String,
+    val recurrence: String,
+
+    val user: UserEntity,
+    val wallet: WalletEntity,
+    val category: CategoryEntity
+)
+data class TransactionWithDetails(
+    @Embedded val transaction: TransactionEntity,
+
+    @Relation(
+        parentColumn = "user_id",
+        entityColumn = "user_id"
+    )
+    val user: UserEntity,
+
+    @Relation(
+        parentColumn = "wallet_id",
+        entityColumn = "wallet_id"
+    )
+    val wallet: WalletEntity,
+
+    @Relation(
+        parentColumn = "category_id",
+        entityColumn = "category_id"
+    )
+    val category: CategoryEntity
+)
+fun TransactionWithDetails.toTransactionFull(): TransactionFull {
+    return TransactionFull(
+        id = transaction.transaction_id,
+        amount = transaction.amount,
+        date = transaction.date,
+        note = transaction.note,
+        currency = transaction.currency,
+        label = transaction.label,
+        image = transaction.image,
+        recurrence = transaction.recurrence,
+
+        user = user,
+        wallet = wallet,
+        category = category
+    )
+}
