5d7d3e38b273463f44dd9cd6b992bdb93d269d71
Dhiren Ruthenavelu <160745030+MCCLUCKY415@users.noreply.github.com>
2025-05-26 09:27:23 +0200

    Feature/achievements/dhiren (#105)
    * Refactor BudgetMainFragment and add BudgetGoalsFragment
    
    *   Added `hello_blank_fragment` string in strings.xml
    *   Removed edit goal icon from `fragment_budget_main.xml`
    * Added `saveButton`, `inputMinGoal`, `inputMaxGoal`, `minValueSpent`, `minValueTotal` views to fragment_budget_main.xml
    * Updated BudgetMainFragment to be `AndroidEntryPoint`
    * Added new fragment `navigation_budget_goals` in mobile_navigation.xml
    * Added action to navigate from BudgetMainFragment to BudgetGoalsFragment in mobile_navigation.xml
    
    * Refactor BudgetMainFragment and add BudgetGoalsFragment
    
    *   Added `hello_blank_fragment` string in strings.xml
    *   Removed edit goal icon from `fragment_budget_main.xml`
    * Added `saveButton`, `inputMinGoal`, `inputMaxGoal`, `minValueSpent`, `minValueTotal` views to fragment_budget_main.xml
    * Updated BudgetMainFragment to be `AndroidEntryPoint`
    * Added new fragment `navigation_budget_goals` in mobile_navigation.xml
    * Added action to navigate from BudgetMainFragment to BudgetGoalsFragment in mobile_navigation.xml
    
    * chore: update version to 2.1.18.3 🚀
    
    * Created Security Page.
    
    * chore: update version to 2.1.18.5 🚀
    
    * Created Privacy Page and made minor changes to security page.
    
    * chore: update version to 2.1.18.7 🚀
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ app/src/main/java/com/synaptix/budgetbuddy/presentation/ui/main/profile/privacy/PrivacyViewModel.kt
@@ -0,0 +1,7 @@
+package com.synaptix.budgetbuddy.presentation.ui.main.profile.privacy
+
+import androidx.lifecycle.ViewModel
+
+class PrivacyViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
